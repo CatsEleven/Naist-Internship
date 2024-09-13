b95b0e5aaea9c9f62b960f825b3ace756b62a5e9
CatsEleven <hatijolove@gmail.com>
2024-09-13 16:02:36 +0900

    final day

--- 05_ovs_env/down.sh
+++ 05_ovs_env/down.sh
@@ -20,7 +20,11 @@ echo $PASSWORD | sudo -S ovs-docker del-port router-c-e eth2 frr-c && \
 echo $PASSWORD | sudo -S ovs-docker del-port router-c-e eth1 frr-e && \
 echo $PASSWORD | sudo -S ovs-docker del-port router-d-e eth2 frr-d && \
 echo $PASSWORD | sudo -S ovs-docker del-port router-d-e eth2 frr-e && \
-echo $PASSWORD | sudo -S ovs-docker del-port router-d-srext eth3 frr-d && \
-echo $PASSWORD | sudo -S ovs-docker del-port srext-ips eth0 ips --ipaddress=2001:db8:1:2::10/64
+# echo $PASSWORD | sudo -S ovs-docker del-port server1-net eth1 server1 --ipaddress=2001:db8:1:1::10/64 && \
+# echo $PASSWORD | sudo -S ovs-docker del-port server1-net eth3 frr-d && \
+# echo $PASSWORD | sudo -S ovs-docker del-port server2-net eth1 server2 --ipaddress=2001:db8:1:2::10/64 && \
+# echo $PASSWORD | sudo -S ovs-docker del-port server2-net eth3 frr-a
+# echo $PASSWORD | sudo -S ovs-docker del-port router-d-srext eth3 frr-d && \
+# echo $PASSWORD | sudo -S ovs-docker del-port srext-ips eth0 ips --ipaddress=2001:db8:1:2::10/64
 
 echo "----------del-port終了----------"
